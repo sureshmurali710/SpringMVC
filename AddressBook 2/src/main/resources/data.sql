18e0d1273a587b9ecb630f107c0450b4d857804c
Suresh Murali <54928199+sureshmurali710@users.noreply.github.com>
2023-05-26 17:15:47 -0400

    Add files via upload

---
+++ AddressBook 2/src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into address(id, clientname,state,pincode)
+values(1, 'CFx', 'New York',1002);
+
+insert into address(id, clientname,state,pincode)
+values(2, 'CFx', 'New Jersey',2003);
+
+insert into address(id, clientname,state ,pincode)
+values(3, 'CFx', 'London',37833);
